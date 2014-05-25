8f9aa7f8dcca5162a8051a58e436e0da7059bc2c
Erik Blas <erik.blas@socrata.com>
2014-05-24 21:37:28 -0700

    removes cached files

--- .cask/24.3.1/bootstrap/git-20140128.241/git-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; git-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("git-pkg.el" "git.el") (21235 55501 840245
-;;;;;;  0))
-
-;;;***
-
-(provide 'git-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; git-autoloads.el ends here
